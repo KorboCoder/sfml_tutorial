d72e994856016fcfeedd020d14fd984ee2adee7d
Daniel Vallesteros <daniel.v@anino.co>
2020-01-04 17:45:47 +0800

    added eagle

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,11 +4,19 @@ class Game {
            void             run();
        private:
            void             processEvents();
-           void             update();
+           void             update(sf::Time);
            void             render();
+           void             handlePlayerInput(sf::Keyboard::Key, bool);
        private:
            sf::RenderWindow mWindow;
-           sf::CircleShape  mPlayer;
+           sf::Texture mTexture;
+           sf::Sprite  mPlayer;
+           bool mIsMovingUp = false;
+           bool mIsMovingDown = false;
+           bool mIsMovingLeft = false;
+           bool mIsMovingRight = false;
+           const float PlayerSpeed = 300.0f;
+           const sf::Time TimePerFrame = sf::seconds(1.f / 60.f);
 };
 
 int main() {
@@ -19,13 +27,26 @@ int main() {
 Game::Game()
 : mWindow(sf::VideoMode(640, 480), "SFML Application") , mPlayer()
 {
-	mPlayer.setRadius(40.f); mPlayer.setPosition(100.f, 100.f); mPlayer.setFillColor(sf::Color::Cyan);
+    mWindow.setFramerateLimit(60);
+    if (!mTexture.loadFromFile("src/Media/Textures/Eagle.png")) {
+     // Handle loading error
+   }
+   mPlayer.setTexture(mTexture);
+   mPlayer.setPosition(100.f, 100.f);
 }
 void Game::run()
 {
+    sf::Clock clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
 	while (mWindow.isOpen()) {
+
         processEvents();
-        update();
+        timeSinceLastUpdate += clock.restart();
+        while (timeSinceLastUpdate > TimePerFrame) {
+            timeSinceLastUpdate -= TimePerFrame;
+            processEvents();
+            update(TimePerFrame);
+        }
         render();
 	}
 }
@@ -34,15 +55,52 @@ void Game::processEvents()
 {
 	sf::Event event;
 	while (mWindow.pollEvent(event)) {
-		if (event.type == sf::Event::Closed) mWindow.close();
+		switch (event.type) {
+            case sf::Event::KeyPressed:
+                handlePlayerInput(event.key.code, true);
+                break;
+            case sf::Event::KeyReleased:
+                handlePlayerInput(event.key.code, false);
+                break;
+            case sf::Event::Closed:
+                mWindow.close();
+                break;
+            default:
+                break;
+        }
 	}
 }
 
-void Game::update()
+void Game::update(sf::Time deltaTime)
 {
+    sf::Vector2f movement(0.f, 0.f);
+    if (mIsMovingUp)
+        movement.y -= PlayerSpeed;
+    if (mIsMovingDown)
+        movement.y += PlayerSpeed;
+    if (mIsMovingLeft)
+        movement.x -= PlayerSpeed;
+    if (mIsMovingRight)
+        movement.x += PlayerSpeed;
+
+    mPlayer.move(movement*deltaTime.asSeconds());
 }
 
  void Game::render()
 {
-	mWindow.clear(); mWindow.draw(mPlayer); mWindow.display();
+	mWindow.clear();
+    mWindow.draw(mPlayer);
+    mWindow.display();
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+    if (key == sf::Keyboard::W)
+        mIsMovingUp = isPressed;
+    else if (key == sf::Keyboard::S)
+        mIsMovingDown = isPressed;
+    else if (key == sf::Keyboard::A)
+        mIsMovingLeft = isPressed;
+    else if (key == sf::Keyboard::D)
+        mIsMovingRight = isPressed;
 }
